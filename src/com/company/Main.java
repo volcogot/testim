ec5e6b5d335071eea982242b57d1e4e06fc1e129
volcogot <72007738+volcogot@users.noreply.github.com>
2020-12-20 15:37:03 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Second Commit");
-        System.out.println("Changes on IDE Intellij Idea");
+        System.out.println("Changes on GitHub");
     }
 }
